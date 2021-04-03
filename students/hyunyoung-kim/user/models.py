73a21f98ad5fe2fc350a83cced35028d3eda399b
hyun0k <hyunyoung9310@gmail.com>
2021-04-03 22:41:47 +0900

    [FIX]:model 수정

--- students/hyunyoung-kim/user/models.py
+++ students/hyunyoung-kim/user/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.CharField(default='', max_length=300, null=False)
+    email    = models.CharField(default='', max_length=300, null=False)
+    name     = models.CharField(default='', max_length=45, null=False)
+    nickname = models.CharField(default='', max_length=45, null=False)
     password = models.CharField(default='', max_length=300, null=False)
 
     class Meta:
