861992dbf043debf7f8f990876f3c19e86d7f4c7
min.ming_ <74173368+z132305@users.noreply.github.com>
2021-04-05 15:42:22 +0900

    회원가입 전화번호, 이메일 정규표현식으로 변경

--- students/sumin-hwang/westagram/users/models.py
+++ students/sumin-hwang/westagram/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    mobile_number = models.IntegerField(unique=True, null=True)
+    mobile_number = models.CharField(max_length=50, unique=True, null=True)
     email         = models.EmailField(max_length=200, unique=True, null=True)
     name          = models.CharField(max_length=100)
     user_name     = models.CharField(max_length=100, unique=True)
--- students/sumin-hwang/westagram/users/views.py
+++ students/sumin-hwang/westagram/users/views.py
@@ -1,9 +1,9 @@
 import json
-#import re
+import re
 
 from django.http import JsonResponse
 from django.views import View
-#from django.db.models import Q
+from django.db.models import Q
 
 from users.models import User
 
@@ -16,10 +16,10 @@ class SignUpView(View):
         email         = data.get('email', None)
         name          = data.get('name', None)
         user_name     = data.get('user_name', None)
-        password      = data.get('password', None) 
+        password      = data.get('password', None)
         
-        #mobile_number_form = re.compile('[0-9]{3}-[0-9]{4}-[0-9]{4}', mobile_number)
-        #email_form         = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
+        mobile_number_form = re.compile('[0-9]{3}-[0-9]{3,4}-[0-9]{4}')
+        email_form         = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
         if not (
                 (mobile_number or email)
@@ -29,31 +29,40 @@ class SignUpView(View):
                 ):
             return JsonResponse({'MESSAGE':'INVALID_KEY'}, status=400)
         
-        #if mobile_number:
-        #    if not mobile_number_form.match(str(mobile_number_form), mobile_number):
-        #        return JsonResponse({'MESSAGE':'INVALID_MOBILE_NUMBER'}, status=400)
+        elif mobile_number:
+            print(mobile_number_form.match(mobile_number))
+            if not mobile_number_form.match(mobile_number):
+               return JsonResponse({'MESSAGE':'INVALID_MOBILE_NUMBER'}, status=400)
+            elif User.objects.filter(mobile_number = data['mobile_number']):
+               return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
 
-        #if email:
-        #    if not email_form.match(str(email_form), email):
-        #        return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=400)
+        elif email:
+           if not email_form.match(str(email)):
+               return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=400)
+           elif User.objects.filter(email = data['email']):
+               return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
         
-        if mobile_number:
-            if '-' in data['mobile_number']:
-                return JsonResponse({'MESSAGE':'INVALID_MOBILE_NUMBER'}, status=400)
-            if User.objects.filter(mobile_number = data['mobile_number']):
-                return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
+        # if mobile_number:
+        #     if '-' in data['mobile_number']:
+        #         return JsonResponse({'MESSAGE':'INVALID_MOBILE_NUMBER'}, status=400)
+        #     if User.objects.filter(mobile_number = data['mobile_number']):
+        #         return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
         
-        if email:
-            if ('@' or '.') not in data['email']:
-                return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=400)
-            if User.objects.filter(email = data['email']):
-                return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
+        # if email:
+        #     if ('@' or '.') not in data['email']:
+        #         return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status=400)
+        #     if User.objects.filter(email = data['email']):
+        #         return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
+
         
         if User.objects.filter(user_name = data['user_name']):
             return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
+
         
         if len(data['password']) < PASSWORD_MINIMUM_LENGTH:
             return JsonResponse({'MESSAGE':'PASSWORD_VALIDATION_ERROR'}, status=400)
+
+        print(email)
 
         User.objects.create(
                 mobile_number = mobile_number,
@@ -62,17 +71,53 @@ class SignUpView(View):
                 user_name = user_name,
                 password = password,
                 )
+
         return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
 
 class SignInView(View):
     def post(self, request):
         data = json.loads(request.body)
-        mobile_number = data.get('mobile_number', None)
-        email         = data.get('email', None)
+        login_id      = data.get('id', None)
+        #mobile_number = data.get('mobile_number', None)
+        #email         = data.get('email', None)
         password      = data.get('password', None)
 
-        #if User.objects.filter(mobile_number = data['mobile_number']).exists():
-        if User.password == data['password']:
+        #if not login_id or password:
+        #    return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
+        #if User.objects.filter(mobile_number = mobile_number).exists():
+        #    user = User.objects.get(mobile_number = data['mobile_number'])
+        #    if user.password == password:
+        #        return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+        #    else:
+        #        return JsonResponse({'MESSAGE':'PASSWORD_ERROR'}, status=401)
+        #else:
+        #    return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+        #if User.objects.filter(email = email).exists():
+        #    user = User.objects.get(email = data['email'])
+        #    if user.password == password:
+        #        return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+        #    else:
+        #        return JsonResponse({'MESSAGE':'PASSWORD_ERROR'}, status=401)
+        #else:
+        #    return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+        if not login_id or password:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+        else :
+            if not User.objects.filter(
+                    Q(mobile_number = login_id) |
+                    Q(email = login_id) |
+                    Q(user_name = login_id)
+                    ).exists():
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+            user = User.objects.get(
+                    Q(mobile_number = data['mobile_number'])|
+                    Q(email = data['email']) |
+                    Q(user_name = login_id)
+                    )
+            if user.password != password:
+                return JsonResponse({'MESSAGE':'PASSWORD_ERROR'}, status=401)
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
-        else:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=401)
+
