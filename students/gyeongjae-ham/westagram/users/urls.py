4083e3947e02553649f81c11bd9b96426804262b
GyeongJae <gyeongjae.h.dev@gmail.com>
2021-04-05 15:46:39 +0900

    update: views.py에 비밀번호 조건 추가

--- students/gyeongjae-ham/westagram/users/urls.py
+++ students/gyeongjae-ham/westagram/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import CreateUserView
+from .views import CreateUserView, LoginView
 
 urlpatterns = [
-    path('/createuser', CreateUserView.as_view())
+    path('/createuser', CreateUserView.as_view()),
+    path('/login', LoginView.as_view())
 ]
--- students/gyeongjae-ham/westagram/users/views.py
+++ students/gyeongjae-ham/westagram/users/views.py
@@ -17,6 +17,8 @@ class CreateUserView(View):
                 return JsonResponse({'MESSAGE':'이미 등록된 번호입니다.'}, status=400)
             elif not '@' in data['email'] or not '.' in data['email']:
                 return JsonResponse({'MESSAGE':'유효한 이메일 형식이 아닙니다.'}, status=400)
+            elif len(data['password']) < 8:
+                return JsonResponse({'MESSAGE':'비밀번호는 8자리 이상입니다.'}, status=400)
 
             User.objects.create(
             name = data['name'],
@@ -30,3 +32,18 @@ class CreateUserView(View):
             return JsonResponse({"MESSAGE": "KEY_ERROR"}, status=400)
 
         return JsonResponse({'MESSAGE': 'SUCCESS'}, status=200)
+
+class LoginView(View):
+    def get(self, request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(email=data['email']).exists() == False:
+                return JsonResponse({'MESSAGE':'INVALIED_USER'}, status=401)
+            elif User.objects.filter(password=data['password']).exists() == False:
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+            elif User.objects.filter(email=data['email']).exists() == True:
+                User.objects.get(email=data['email'])
+                if User.objects.filter(password=data['password']).exists() == True:
+                    return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
+        except:
+            return JsonResponse( {'MESSAGE': "KEY_ERROR"}, status=400)
