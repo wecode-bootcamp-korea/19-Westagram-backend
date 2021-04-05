8c260c0de2c5ebc4b95f7dbfa48007c2dd3cd2a2
ulr0 <ulr0xb@gmail.com>
2021-04-06 01:52:07 +0900

    1. email, password validation
    2. 중복 email 가입 불가능 기능 추가

--- students/deukyeong-seo/westagram/user/views.py
+++ students/deukyeong-seo/westagram/user/views.py
@@ -6,16 +6,29 @@ from django.http  import JsonResponse
 from .models import User
 
 class SignUp(View):
+    
     def post(self, request):
-        data = json.loads(request.body)
-
-        try:    
         
-            name     = data['name']
-            email    = data['email']
-            password = data['password']
+        data = json.loads(request.body)
+        MINIMUM_PASSWORD_LENGTH = 8
+        
+        try:
 
-            User.objects.create(name=name, email=email, password=password)
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message':'DUPLICATE_EMAIL'}, status=400)
+
+            if '@' not in data['email'] or '.' not in data['email']:
+                return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
+            
+            if len(data['password']) < MINIMUM_PASSWORD_LENGTH:
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
+
+            User.objects.create(
+                    name     = data['name'],
+                    email    = data['email'],
+                    password = data['password']
+                    )
+
 
             return JsonResponse({'message':'SUCCESS'}, status=201)
 
