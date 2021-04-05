e10dda6986bb7d85c6a371ff57d6d890bb66c340
jeewonn <jeewonn0504@gmail.com>
2021-04-05 17:58:37 +0900

    체크포인트2 수정2

--- students/jeewon-shin/westagram/user/views.py
+++ students/jeewon-shin/westagram/user/views.py
@@ -6,16 +6,14 @@ from user.models  import User
 
 class SignupView(View):
     def post(self, request):
-        data  = json.loads(request.body)
-        if data['email'] == '' or data['password'] == '':
-            return JsonResponse({'MESSGAGE': 'KEY_ERROR'}, status=400)
-        elif '@' and '.' not in data['email']:
-            return JsonResponse({'MESSGAGE': 'This is not valid email.'}, status=400)
-        elif User.objects.filter(email= data['email']).exists():
-            return JsonResponse({'MESSGAGE': 'This email is already used.'}, status=400)
-        elif len(data['password']) <= 8:
-            return JsonResponse({'MESSGAGE': 'This is not valid password.'}, status=400)
-        else:
+        try:
+            data  = json.loads(request.body)
+            if '@' and '.' not in data['email']:
+                return JsonResponse({'MESSGAGE': 'This is not valid email.'}, status=400)
+            elif User.objects.filter(email= data['email']).exists():
+                return JsonResponse({'MESSGAGE': 'This email is already used.'}, status=400)
+            elif len(data['password']) <= 8:
+                return JsonResponse({'MESSGAGE': 'This is not valid password.'}, status=400)
             user = User.objects.create(
                 email           = data['email'],
                 password        = data['password'],
@@ -23,4 +21,5 @@ class SignupView(View):
                 phone_number    = data['phone_number']
             )
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
-
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEYERROR'}, status=400)
