670054c80bd37390495961f6baf5aa4d258e4cab
ulr0 <ulr0xb@gmail.com>
2021-04-05 15:04:53 +0900

    models.py 작성 : users 테이블 생성

--- students/deukyeong-seo/westagram/user/models.py
+++ students/deukyeong-seo/westagram/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name     = models.CharField(max_length=45)
+    email    = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'users'
