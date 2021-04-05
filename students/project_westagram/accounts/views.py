e62bc0f15184d3eb1eaf2c736018d7473107ed3f
john Youn <johnyoun@JohnyWhisky.local>
2021-04-05 15:06:50 +0900

    message: sign up Views 완료

--- students/project_westagram/accounts/views.py
+++ students/project_westagram/accounts/views.py
@@ -14,33 +14,34 @@ class SignupView(View):
         accounts = Accounts.objects.all()
         
         id_validation    = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        pw_validation    = re.compile('')
+        pw_validation    = re.compile('[{(<`~!@#$%^&*,./?;:>)}]+')
         phone_validation = re.compile('[0-9]')
         
         # ID Validation check. 1st if > 중복 확인, 2nd if > E-mail 형식 확인
         if Accounts.objects.filter(user_id = data['user_id']).exists():
-            if not id_validation.match(data['user_id']):
-                return JsonResponse({'message': 'KEY_ERROR Invalid ID'}, status = 400)
             return JsonResponse({'message': 'KEY_ERROR ID already exist'}, status = 400)
+        if not id_validation.match(data['user_id']):
+            return JsonResponse({'message': 'KEY_ERROR Invalid ID'}, status = 400)
 
-        # Password Validation check. 1st if > 비밀번호 길이 확인, 2nd if > 비밀번호 복잡도 확인
-        if pw_validation.match(data['user_pw']):
-            if len(data['user_pw']) < 8:
-                return JsonResponse({'message': 'KEY_ERROR Password too short!'}, status = 400)
-            return 
-        # Phone number Validation check
-        # 1st if > 전화번호 길이 확인, 2nd if > 전화번호 형식 확인, 3rd if > 전화번호 중복 확인
-        if len(data['user_phone'] > 11) or len(data['user_phone']) < 10:
-            if phone_validation.match(int(data['user_phone'])):
-                if Accounts.objects.filter(user_phone = data['user_phone']).exists():
-                    return JsonResponse({'message': 'KEY_ERROR PHONE number already exist'}, status = 400)
-                return JsonResponse({'message': 'KEY_ERROR Wrong PHONE number'}, status = 400)
+        # Password Validation check. 1st if > 비밀번호 길이 확인, 2nd if > 비밀번호 복잡도 확인1, 3rd if > 비밀번호 복잡도 확인2
+        if len(data['user_pw']) < 8 or len(data['user_pw']) > 30:
+            return JsonResponse({'message': 'KEY_ERROR Password too short!'}, status = 400)
+        if not re.findall('[a-zA-Z]+', data['user_pw']) and not re.findall('[0-9]+', data['user_pw']):
+            return JsonResponse({'message': 'KEY_ERROR Invalid Password(각 하나 이상의 소문자, 대문자, 숫자를 포함하세요)'}, status = 400)
+        if not re.findall('[<`~!@#$%^&*,./?;:>()]+', data['user_pw']):
+            return JsonResponse({'message': 'KEY_ERROR Invalid Password(하나 이상의 특수문자를 포함하세요)'}, status = 400)
+        
+        # Phone number Validation check. 1st if > 전화번호 길이 및 전화번호 형식 확인, 2nd if > 전화번호 중복 확인
+        if (len(data['user_phone']) > 11 or len(data['user_phone']) < 10) and not phone_validation.match(data['user_phone']):
+            return JsonResponse({'message': 'KEY_ERROR Wrong PHONE number'}, status = 400)
+        if Accounts.objects.filter(user_phone = data['user_phone']).exists():
+            return JsonResponse({'message': 'KEY_ERROR PHONE number already exist'}, status = 400)
 
         Accounts.objects.create(user_id       = data['user_id'],
                                 user_pw       = data['user_pw'],
-                                user_name     = data["user_name"],
+                                user_name     = data['user_name'],
                                 user_phone    = data['user_phone'],
-                                user_nickname = data['user_nickname'])
+                                )
             
         return JsonResponse({"message": "Sign up complete!"}, status = 201)
 
