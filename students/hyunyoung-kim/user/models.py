ee7375c0dc5683a109dce468f83849604efc02fe
hyun0k <hyunyoung9310@gmail.com>
2021-04-05 14:16:24 +0900

    [FIX]:필드 default값 제거

--- students/hyunyoung-kim/user/models.py
+++ students/hyunyoung-kim/user/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    email    = models.CharField(default='', max_length=300, null=False)
-    name     = models.CharField(default='', max_length=45, null=False)
-    nickname = models.CharField(default='', max_length=45, null=False)
-    password = models.CharField(default='', max_length=300, null=False)
+    email    = models.CharField(max_length=300)
+    name     = models.CharField(max_length=45)
+    nickname = models.CharField(max_length=45)
+    password = models.CharField(max_length=300)
 
     class Meta:
         db_table = 'users'
