c5b2babbd0ac145653f1ea59ad774796d18aaa16
hyun0k <hyunyoung9310@gmail.com>
2021-04-05 20:55:07 +0900

    [CHORE]:View 경로 수정

--- students/hyunyoung-kim/user/urls.py
+++ students/hyunyoung-kim/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views  import SignUp
+from user.views  import SignUp, SingIn
 
 urlpatterns = [
     path('/signup', SignUp.as_view()),
+    path('/signin', SingIn.as_view())
 ]
