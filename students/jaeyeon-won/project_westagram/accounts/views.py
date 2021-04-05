453feba8a35a1eaba121de1ad1a9c438aee8553e
dnjs0718 <mystund@naver.com>
2021-04-05 19:03:40 +0900

    회원가입 수정끝

--- students/jaeyeon-won/project_westagram/accounts/views.py
+++ students/jaeyeon-won/project_westagram/accounts/views.py
@@ -11,24 +11,29 @@ class SignupView(View):
         data = json.loads(request.body)
 
         try:
-            name                    = data['name']
-            phone_number            = data['phone_number']
-            email                   = data['email']
-            username                = data ['username']
-            password                = data['password']
-            email_vaildation        = re.match('[a-zA-Z0-9._+-]+@[a-z0-9-]+\.[a-z.]+',email)
-            password_vaildation     = re.match('^(?=.*[a-zA-Z0-9.,-]).{8,}$',password)
+            name                = data['name']
+            phone_number        = data['phone_number']
+            email               = data['email']
+            username            = data ['username']
+            password            = data['password']
+            email_vaildation    = re.match('[a-zA-Z0-9._+-]+@[a-z0-9-]+\.[a-z.]+',email)
+            password_vaildation = re.match('^(?=.*[a-zA-Z0-9.,-]).{8,}$',password)
             
             if name =='' or (phone_number=='' and email=='') or username == '' or password =='':
                 return JsonResponse({'message' : 'Check your Input'}, status = 400)
+
             if not email_vaildation:
                 return JsonResponse({'messsage' : 'Check your email'}, status = 400)
+
             if not password_vaildation:
                 return JsonResponse({'message' : 'Check your password'}, status = 400)
+
             if User.objects.filter(phone_number = phone_number).exists():
                 return JsonResponse({'message' : 'Already exists phone_number'}, status = 400)
+
             if User.objects.filter(username = username).exists():
                 return JsonResponse({'message' : 'Already exists username'}, status = 400)
+
             if User.objects.filter(email = email).exists():
                 return JsonResponse({'message' : 'Already exists email'}, status = 400)
 
@@ -41,7 +46,7 @@ class SignupView(View):
                 )
             return JsonResponse({'message' : 'All done!'}, status = 201)
         except KeyError:
-            return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+            return JsonResponse({'message':'KEY ERROR'}, status = 400)
 
 class SigninView(View):
     def post(self,request):
@@ -55,11 +60,15 @@ class SigninView(View):
             if User.objects.filter(email=email).exists() or User.objects.filter(username=username).exists() or User.objects.filter(phone_number=phone_number).exists():    
                 if password =='':
                     return JsonResponse({'message': 'Check your Input'},status = 401)
+
                 if User.objects.filter(password=password).exists():
                     return JsonResponse({'message': 'All done!'}, status = 200)
+
                 else:
                     return JsonResponse({'message': 'Check your password'}, status = 401)
+
             else:
                 return JsonResponse({'message': 'Check your ID'}, status = 401)
+
         except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'},status = 400)
+            return JsonResponse({'message': 'KEY ERROR'},status = 400)
