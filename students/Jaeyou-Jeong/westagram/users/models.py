6d29d597b4c5b0b1d3bf12628b4c54d05701aa9e
deonii <fishingman99@gmail.com>
2021-04-05 16:19:11 +0900

    수정3) 테이블명 변경, 비밀번호 길이 상수 생성, 에러 메세지 수정

--- students/Jaeyou-Jeong/westagram/users/models.py
+++ students/Jaeyou-Jeong/westagram/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class Signin(models.Model):
+class UserInfo(models.Model):
     email        = models.EmailField(max_length=200)
     password     = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     user_name    = models.CharField(max_length=100)
 
     class Meta:
-        db_table = 'signin'
+        db_table = 'user_info'
--- students/Jaeyou-Jeong/westagram/users/views.py
+++ students/Jaeyou-Jeong/westagram/users/views.py
@@ -3,23 +3,24 @@ import json
 from django.http import JsonResponse
 from django.views import View
 
-from users.models import Signin
+from users.models import UserInfo
 from users.checks import email_validation
 
 class Sign(View):
     def post(self, request):
         data = json.loads(request.body)
+        password_length = 8
         try:
-            if len(data['password']) < 8:
-                return JsonResponse({"message": "비밀번호는 8자리 이상으로 만들어 주세요."}, status=400)
+            if len(data['password']) < password_length:
+                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
             elif not  email_validation(data['email']):
-                return JsonResponse({"message": "이메일 양식에 맞춰주세요."}, status=400)
-            elif Signin.objects.filter(email=data['email']).exists()\
-                    or Signin.objects.filter(user_name=data['user_name']).exists()\
-                    or Signin.objects.filter(phone_number=data['phone_number']).exists():
-                return JsonResponse({"message": "이미 존재하는 사용자 입니다."}, status=400)
+                return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
+            elif UserInfo.objects.filter(email=data['email']).exists()\
+                    or UserInfo.objects.filter(user_name=data['user_name']).exists()\
+                    or UserInfo.objects.filter(phone_number=data['phone_number']).exists():
+                return JsonResponse({"message": "EXISTS_USER"}, status=400)
 
-            Signin.objects.create(
+            UserInfo.objects.create(
                     email        = data['email'],
                     password     = data['password'],
                     phone_number = data['phone_number'],
