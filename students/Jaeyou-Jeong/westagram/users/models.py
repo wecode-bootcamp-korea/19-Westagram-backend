b35c34e58010ee2c3c4671b883551547a922425d
deonii <fishingman99@gmail.com>
2021-04-05 20:00:25 +0900

    수정5) 상수명 대문자로 변경, 클래스명 변경, 유저정보 생성후 리턴, views.py elif문 수정

--- students/Jaeyou-Jeong/westagram/users/models.py
+++ students/Jaeyou-Jeong/westagram/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class UserInfo(models.Model):
+class User(models.Model):
     email        = models.EmailField(max_length=200)
     password     = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
--- students/Jaeyou-Jeong/westagram/users/views.py
+++ students/Jaeyou-Jeong/westagram/users/views.py
@@ -3,31 +3,30 @@ import json
 from django.http import JsonResponse
 from django.views import View
 
-from users.models import UserInfo
+from users.models import User
 from users.checks import email_validation
 
 class Sign(View):
     def post(self, request):
         data = json.loads(request.body)
-        password_length = 8
+        PASSWORD_LENGTH = 8
         try:
-            if len(data['password']) < password_length:
+            if len(data['password']) < PASSWORD_LENGTH:
                 return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
-            elif not  email_validation(data['email']):
+            if not  email_validation(data['email']):
                 return JsonResponse({"message": "INVALID_EMAIL"}, status=400)
-            elif UserInfo.objects.filter(email=data['email']).exists()\
-                    or UserInfo.objects.filter(user_name=data['user_name']).exists()\
-                    or UserInfo.objects.filter(phone_number=data['phone_number']).exists():
+            if User.objects.filter(email=data['email']).exists()\
+                    or User.objects.filter(user_name=data['user_name']).exists()\
+                    or User.objects.filter(phone_number=data['phone_number']).exists():
                 return JsonResponse({"message": "EXISTS_USER"}, status=400)
 
-            UserInfo.objects.create(
+            User.objects.create(
                     email        = data['email'],
                     password     = data['password'],
                     phone_number = data['phone_number'],
                     user_name    = data['user_name']
                     )
+            return JsonResponse({"message": "Success"}, status=201)
 
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        return JsonResponse({"message": "Success"}, status=201)
