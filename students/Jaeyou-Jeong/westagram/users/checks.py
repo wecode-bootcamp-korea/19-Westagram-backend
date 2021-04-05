b2fb2ff69bdb13ce658109dd48c14d26bffe1263
deonii <fishingman99@gmail.com>
2021-04-05 16:40:15 +0900

    수정4) 정규식 오타 수정

--- students/Jaeyou-Jeong/westagram/users/checks.py
+++ students/Jaeyou-Jeong/westagram/users/checks.py
@@ -1,7 +1,7 @@
 import re
 
 def email_validation(email):
-    formula = re.compile('^[a-zA-z0-9-_.]+@+[a-zA-z0-9.]+\.+\w+$')
+    formula = re.compile('^[a-zA-Z0-9-_.]+@+[a-zA-Z0-9.]+\.+\w+$')
 
     if formula.match(email):
         return True
