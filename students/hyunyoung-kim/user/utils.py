28cbaf9ba14315803332a6a1cb08a3671b1f47cd
hyun0k <hyunyoung9310@gmail.com>
2021-04-05 19:15:29 +0900

    [FIX]:validate메소드 수정

--- students/hyunyoung-kim/user/utils.py
+++ students/hyunyoung-kim/user/utils.py
@@ -1,15 +1,9 @@
 import re
 
-from .models   import User 
-
-def email_check(email):
-    return re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email) != None 
-
-def password_check(password):
-    return len(password)>=8
-
-def duplicate_email_check(email):
-    return User.objects.filter(email=email).exists() 
+def validate_email(email):
+    return re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email) is not None
     
-def duplicate_nickname_check(nickname):
-    return User.objects.filter(nickname=nickname).exists()
+def validate_password(password):
+    MIN_LENGTH = 8
+    return len(password) >= MIN_LENGTH 
+
