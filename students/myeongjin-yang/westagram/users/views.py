5e5eae44609def6df5c12710294f6e88e634b0fa
audwls624 <78643271+audwls624@users.noreply.github.com>
2021-04-05 20:39:11 +0900

    회원가입 view 수정

--- students/myeongjin-yang/westagram/users/views.py
+++ students/myeongjin-yang/westagram/users/views.py
@@ -11,6 +11,7 @@ class SignUpView(View):
         data               = json.loads(request.body)
         email_check        = re.compile('^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$')
         phonenumber_check  = re.compile('[0-9]{3}[0-9]{4}[0-9]{4}')
+        password_check     = 8
 
         try:
             identity       = data['identity']
@@ -21,44 +22,43 @@ class SignUpView(View):
             if not email_check.match(identity) and not phonenumber_check.match(identity):
                 return JsonResponse({'message':"Invalid ID"}, status=400)
                         
-            elif len(password)<8:
+            if len(password)<password_check:
                 return JsonResponse({'message':"Invalid PW"}, status=400)
 
-            elif User.objects.filter(username=username):
+            if User.objects.filter(username=username):
                 return JsonResponse({'message':"existing username"}, status=400)
             
-            elif User.objects.filter(identity=identity):
+            if User.objects.filter(identity=identity):
                 return JsonResponse({'message':"existing ID"}, status=400)
             
-            else:
-                User.objects.create(identity=data['identity'], password=data['password'], name=data['name'], username=data['username'])
+            User.objects.create(identity=data['identity'], password=data['password'], name=data['name'], username=data['username'])
+            
+            return JsonResponse({'message':"SUCCESS"}, status=200)
         
         except KeyError:
             return JsonResponse({'message':"KEY_ERROR"}, status=400)
         
-        else:
-            return JsonResponse({'message':"SUCCESS"}, status=200)
 
 class SignInView(View):
     def post(self,request):
         data               = json.loads(request.body)
         try:
             if 'identity' in data:
-                indentity  = data['identity']
-                password   = data['password']
+                indentity = data['identity']
+                password  = data['password']
 
                 if User.objects.filter(identity=indentity).exists():
-                    user   = User.objects.get(identity=indentity)
+                    user  = User.objects.get(identity=indentity)
                 else:
                     return JsonResponse({'message':"INVALID_USER"}, status=401)
 
             
             else:
-                username   = data['username']
-                password   = data['password']
+                username  = data['username']
+                password  = data['password']
 
                 if User.objects.filter(username=username).exists():
-                    user   = User.objects.get(username=username)
+                    user  = User.objects.get(username=username)
                 else:
                     return JsonResponse({'message':"INVALID_USER"}, status=401)
             
