57f73237d192d926ea01b94d1d8a3e5fd5f9acee
leebj <redgpcmffld@gmail.com>
2021-04-06 15:14:47 +0900

    회원가입 뷰 수정

--- students/byeongjae-lee/westagram/users/models.py
+++ students/byeongjae-lee/westagram/users/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class User(models.Model):
     
-    user_id  = models.CharField(max_length=50)
+    name  = models.CharField(max_length=20)
     email    = models.CharField(max_length=50)
     password = models.TextField()
-    phone_number = models.CharField(max_length=11)
+    phone_number = models.CharField(max_length=20)
 
     class Meta:
         db_table = 'users'
