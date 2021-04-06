0b2806a740d8699b6342d2a4c15550917470356e
fcfargo <fcfargo90@gmail.com>
2021-04-06 18:27:37 +0900

    django 회원가입 엔드포인트 수정3차

--- students/younghun-kim/project_westagram/users/views.py
+++ students/younghun-kim/project_westagram/users/views.py
@@ -9,24 +9,25 @@ from users.models import User
 class SignupView(View):
     def post(self, request):
         data         = json.loads(request.body)
-        email        = data['email']
-        password     = data['password']
-        name         = data['name']
-        phone_number = data['phone_number']
+        try:
+            email        = data['email']
+            password     = data['password']
+            name         = data['name']
+            phone_number = data['phone_number']
+
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+
+        if not ('@' in email) or not ('.' in email ):
+            return JsonResponse({"message":"Enter a valid useremail"}, status = 400)
+        PASSWORD_LENGTH = 8
+
+        if len(password) < PASSWORD_LENGTH:
+            return JsonResponse({"message":"At least 8 characters"}, status = 400)
 
         if User.objects.filter(email=email).exists():
             return JsonResponse({"message":"Duplicate_Useremail"}, status = 400)
 
-        if not (email and password):
-            return JsonResponse({"message":"KEY_ERROR:Enter password and email"}, status = 400)
-
-        if not ('@' in email) or not ('.' in email ):
-            return JsonResponse({"message":"Enter a valid useremail"}, status = 400)
-        
-        PASSWORD_LENGTH = 8
-        if len(password) < PASSWORD_LENGTH:
-            return JsonResponse({"message":"At least 8 characters"}, status = 400)
-            
         hashed_password  = bcrypt.hashpw(password.encode('UTF-8'), bcrypt.gensalt()).decode()
         User.objects.create(
             email        = email,
@@ -41,7 +42,7 @@ class  LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        if data['email']:
+        if data.get('email'):
             email         = data['email']
             password      = data['password']
 
@@ -58,3 +59,42 @@ class  LoginView(View):
                 return JsonResponse({"message":"INVALID_USER"}, status = 401)
             
             return JsonResponse({"message":"SUCCESS"}, status = 200)
+
+        if data.get('name'):
+            name          = data['name']
+            password      = data['password']
+
+            user          = User.objects.filter(name=name)
+            user_password = user[0].password
+            
+            if not (name and password):
+                return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+
+            if not user.exists():
+                return JsonResponse({"message":"INVALID_USER"}, status = 401)
+            
+            if not bcrypt.checkpw(password.encode('utf-8'), user_password.encode('UTF-8')):
+                return JsonResponse({"message":"INVALID_USER"}, status = 401)
+            
+            return JsonResponse({"message":"SUCCESS"}, status = 200)
+
+
+        if data.get('phone_number'):
+            phone_number  = data['phone_number']
+            password      = data['password']
+
+            user          = User.objects.filter(phone_number=phone_number)
+            user_password = user[0].password
+            
+            if not (phone_number and password):
+                return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+
+            if not user.exists():
+                return JsonResponse({"message":"INVALID_USER"}, status = 401)
+            
+            if not bcrypt.checkpw(password.encode('utf-8'), user_password.encode('UTF-8')):
+                return JsonResponse({"message":"INVALID_USER"}, status = 401)
+            
+            return JsonResponse({"message":"SUCCESS"}, status = 200)
+
+
