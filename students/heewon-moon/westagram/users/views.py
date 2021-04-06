39156a917c521f7e875ec12e23180ecd06fd3d0b
moonimooni <munhw1408@gmail.com>
2021-04-06 17:47:40 +0900

    modify : 로그인시 유저 존재여부 필터링 수정

--- students/heewon-moon/westagram/users/views.py
+++ students/heewon-moon/westagram/users/views.py
@@ -2,12 +2,13 @@ import json
 import bcrypt
 import jwt
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views       import View
+from django.http        import JsonResponse
+from django.db.models   import Q
 
-from .models      import User
-from .validators  import email_validator, password_validator, phone_validator
-from westagram.settings  import SECRET_KEY, ALGORITHM
+from .models            import User
+from .validators        import email_validator, password_validator, phone_validator
+from westagram.settings import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
@@ -52,13 +53,12 @@ class SignInView(View):
 
             account  = data['account']
             password = data['password']
+            
+            user = User.objects.filter(Q(email=account) | Q(phone=account) | Q(nickname=account))
 
-            if email_validator(account):
-                user = User.objects.get(email=account)
-            elif phone_validator(account):
-                user = User.objects.get(phone=account)
-            else:
-                user = User.objects.get(nickname=account)
+            if not user:
+                return JsonResponse({'MESSAGE' : 'INVALID USER'}, status=401)
+            user = user[0]
             
             input_password = password.encode('utf-8')
             db_password = user.password.encode('utf-8')
@@ -69,7 +69,5 @@ class SignInView(View):
             
             return JsonResponse({'MESSAGE' : 'WRONG PASSWORD'}, status=401)
 
-        except User.DoesNotExist:
-            return JsonResponse({'MESSAGE' : 'INVALID USER'}, status=401)
         except KeyError:
             return JsonResponse({'MESSAGE' : 'KEY ERROR'}, status=400)
