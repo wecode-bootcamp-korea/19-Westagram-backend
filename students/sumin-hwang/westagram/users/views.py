895023d26646f440945bddce29deb94b30939ace
min.ming_ <74173368+z132305@users.noreply.github.com>
2021-04-06 17:26:03 +0900

    라인 맞춤

--- students/sumin-hwang/westagram/users/views.py
+++ students/sumin-hwang/westagram/users/views.py
@@ -15,11 +15,11 @@ class SignUpView(View):
     def post(self, request):
         
         try:
-            data = json.loads(request.body)
+            data          = json.loads(request.body)
             mobile_number = data.get('mobile_number', None)
             email         = data.get('email', None)
             name          = data.get('name', None)
-            nickname     = data.get('nickname', None)
+            nickname      = data.get('nickname', None)
             password      = data.get('password', None)
         
             mobile_number_form = re.compile('[0-9]{3}-[0-9]{3,4}-[0-9]{4}')
@@ -42,24 +42,24 @@ class SignUpView(View):
 
             if User.objects.filter(
                     Q(mobile_number = mobile_number) |
-                    Q(email = email) |
-                    Q(nickname = nickname)
+                    Q(email         = email) |
+                    Q(nickname      = nickname)
                     ):
                 return JsonResponse({'MESSAGE':'ALREADY_EXISTS'}, status=400)
 
             if len(data['password']) < PASSWORD_MINIMUM_LENGTH:
                 return JsonResponse({'MESSAGE':'PASSWORD_VALIDATION_ERROR'}, status=400)
 
-            password = data['password'].encode('utf-8')
+            password        = data['password'].encode('utf-8')
             hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
             hashed_password = hashed_password.decode('utf-8')
 
             User.objects.create(
                     mobile_number = mobile_number,
-                    email = email,
-                    name = name,
-                    nickname = nickname,
-                    password = password,
+                    email         = email,
+                    name          = name,
+                    nickname      = nickname,
+                    password      = password,
                     )
 
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
@@ -70,26 +70,26 @@ class SignUpView(View):
 class SignInView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
+            data          = json.loads(request.body)
             password      = data.get('password', None)
             mobile_number = data.get('mobile_number', None)
-            email = data.get('email', None)
-            nickname = data.get('nickname', None)
+            email         = data.get('email', None)
+            nickname      = data.get('nickname', None)
 
             if not ((mobile_number or email or nickname) and password):
                 return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
         
             if not User.objects.filter(
                     Q(mobile_number = mobile_number) |
-                    Q(email = email) |
-                    Q(nickname = nickname)
+                    Q(email         = email) |
+                    Q(nickname      = nickname)
                     ).exists():
                 return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
                 
             user = User.objects.get(
                     Q(mobile_number = mobile_number) |
-                    Q(email = email) |
-                    Q(nickname = nickname)
+                    Q(email         = email) |
+                    Q(nickname      = nickname)
                     )
 
             if user.password != password:
