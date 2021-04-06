555d4738aef1fa9b47ededce96ec825d3a766415
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-04-06 21:00:23 +0900

    회원가입 엔드포인트 #2

--- students/yunhyung-lee/project_westagram/users/models.py
+++ students/yunhyung-lee/project_westagram/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     email       = models.CharField(max_length=100)
     password    = models.CharField(max_length=30)
     phone_num   = models.CharField(max_length=30)
-    user_name   = models.CharField(max_length=30)
+    name        = models.CharField(max_length=30)
 
     class Meta:
         db_table = 'users'
--- students/yunhyung-lee/project_westagram/users/views.py
+++ students/yunhyung-lee/project_westagram/users/views.py
@@ -3,7 +3,6 @@ import bcrypt
 
 from django.http    import JsonResponse
 from django.views   import View
-
 from .models        import User
 
 
@@ -26,40 +25,21 @@ class SignUpView(View):
             if User.objects.filter(phone_num=data['phone_num']).exists():
                 return JsonResponse({'MESSAGE' : 'DUPLICATED PHONE_NUM'}, status=400)
             
-            if User.objects.filter(user_name=data['user_name']).exists():
-                return JsonResponse({'MESSAGE' : 'DUPLICATED USER_NAME'}, status=400)
+            if User.objects.filter(user_name=data['name']).exists():
+                return JsonResponse({'MESSAGE' : 'DUPLICATED NAME'}, status=400)
 
             User.objects.create(
-                    user_name   = data['user_name'],
+                    name        = data['name'],
                     phone_num   = data['phone_num'],
                     password    = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
                     email       = data['email']
             )
         
             return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=201)
+
         except KeyError:
             return JsonResponse({'MESSAGE' : 'KEY ERROR'}, status=400)
 
-
-#class SignInView(View):
-#    def post(self, request):
-#        try:
-#            data     = json.loads(request.body)
-#            password = data['password']
-#            email    = data['email']
-
-#            if not email or not password:
-#                return JsonResponse({'MESSAGE' : 'NO VALUE ENTERED'})
-
-#            if not User.objects.filter(email=email).exists():
-#                signin_user = Use.objects.get(email=email)
-        
-#                if bcrypt.checkpw(password.encode('utf-8'), signin_user.password.encode('utf-8')):
-#                    return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=200)
-
-#            return JsonResponse({'MESSAGE' : 'INVALID USER'}, status=401)
-#        except KeyError:
-#            return JsonResponse({'MESSAGE' : 'KEY ERROR'}, status=400)
 
 class SignInView(View):
     def post(self, request):
@@ -78,8 +58,6 @@ class SignInView(View):
             
             if bcrypt.checkpw(password.encode('utf-8'), signin_user.password.encode('utf-8')):
                 return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=200)
+        
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEY ERROR'}, status=400)
-
-
-
