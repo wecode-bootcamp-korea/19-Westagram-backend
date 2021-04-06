d130d64e70b52577242ae8095e304a4e258b6bd6
john Youn <johnyoun@JohnyWhisky.local>
2021-04-06 16:23:53 +0900

    message: nickname 추가 및 추가적인  log in 로직 구현

--- students/project_westagram/accounts/models.py
+++ students/project_westagram/accounts/models.py
@@ -5,6 +5,7 @@ class Accounts(models.Model):
     email    = models.CharField(max_length = 50)
     password = models.CharField(max_length = 255)
     name     = models.CharField(max_length = 16)
+    nickname = models.CharField(max_length = 30)
     phone    = models.CharField(max_length = 11)
 
     class Meta:
--- students/project_westagram/accounts/views.py
+++ students/project_westagram/accounts/views.py
@@ -1,5 +1,4 @@
 import json
-from json import decoder
 import bcrypt
 import jwt
 import re
@@ -28,9 +27,9 @@ class SignupView(View):
         
         try:
             if accounts.filter(email = data['email']).exists():
-                return JsonResponse({'message': f"E-mail({data['email']}) already exist"}, status = 400)
+                return JsonResponse({'message': "Already exist email"}, status = 400)
             if not email_validation.match(data['email']):
-                return JsonResponse({'message': f"{data['email']} is Invalid E-mail"}, status = 400)
+                return JsonResponse({'message': "Invalid email"}, status = 400)
 
             if len(data['password']) < min_password or len(data['password']) > max_password:
                 return JsonResponse({'message': 'Password too short!'}, status = 400)
@@ -41,21 +40,23 @@ class SignupView(View):
             encrypted_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
             
             if name_validation.search(data['name']):
-                return JsonResponse({'message': f"{data['name']} is Invalid Name"}, status = 400)
+                return JsonResponse({'message': "Invalid Name"}, status = 400)
+            
+            if accounts.filter(nickname = data['nickname']).exists():
+                return JsonResponse({'message': 'Already exist nickname'}, status = 400)
             
             if len(data['phone']) > max_phone or len(data['phone']) < min_phone or not phone_validation.match(data['phone']):
-                return JsonResponse({'message': f"{data['phone']} is Wrong PHONE number"}, status = 400)
+                return JsonResponse({'message': "Invalid phone number"}, status = 400)
             if accounts.filter(phone = data['phone']).exists():
-                return JsonResponse({'message': f"PHONE number({data['phone']}) already exist"}, status = 400)
-
-            
+                return JsonResponse({'message': "Already exist phone"}, status = 400)
             
             Accounts.objects.create(email    = data['email'],
                                     password = encrypted_password,
                                     name     = data['name'],
+                                    nickname = data['nickname'],
                                     phone    = data['phone'],
                                     )
-                
+            
             return JsonResponse({"message": "Sign up complete!"}, status = 201)
         
         except KeyError as error_source:
@@ -67,44 +68,33 @@ class LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
         
-        email_validation = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        phone_validation = re.compile('[0-9]')
+        email_validation    = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        password_validation = re.compile('[(<`~!@#$%^&*,./?;:>)_]+')
+        phone_validation    = re.compile('[0-9]+')
+        
+        accounts = Accounts.objects.all()
         
         try:
-            if request_email := data.get('email'):
-                if not email_validation.match(request_email):
-                    return JsonResponse({'message': f'Invalid USER, {request_email} is not email form'}, status = 401)
-                if Accounts.objects.filter(email = request_email).exists():
-                    if Accounts.objects.get(email = request_email).password == data['password']:
-                        return JsonResponse({'message': 'Log in SUCCESS'}, status = 200)
-                    else:
-                        return JsonResponse({'message': 'Wrong PASSWORD'}, status = 200)
-                else:
-                    return JsonResponse({'message': f'Invalid USER, {request_email} doesn\'t exist'}, status = 401)
+            # if request_id := data.get('email'):
+            #     if bcrypt.checkpw(data['password'].encode("utf-8"), Accounts.objects.get(email = request_id).password.encode('utf-8')):
+            #         return JsonResponse({'message': 'Log in SUCCESS'}, status = 400)
+            
+            # if request_id := data.get('phone'):
+            #     if bcrypt.checkpw(data['password'].encode("utf-8"), Accounts.objects.get(name = request_id).password.encode('utf-8')):
+            #         return JsonResponse({'message': 'Log in SUCCESS'}, status = 400)
+            
+            if accounts.filter(email = data["account"]).exists():
+                request_id = accounts.get(email = data.get('account'))
+            if accounts.filter(nickname = data['nickname']).exists():
+                request_id = accounts.get(email = data.get('account'))
+            if accounts.filter(phone = data['phone']).exists():
+                request_id = accounts.get(phone = data.get('account'))
                 
-            if request_name := data.get('name'):
-                if Accounts.objects.filter(name = request_name).exists():
-                    if data["password"] == Accounts.objects.get(name = request_name).password:
-                        return JsonResponse({'message': 'Log in SUCCESS'}, status = 200)
-                    else:
-                        return JsonResponse({'message': 'Wrong PASSWORD'}, status = 200)
-                else:
-                    return JsonResponse({'message': f'Invalid USER, {request_name} doesn\'t exist'}, status = 401)
-                
-            if request_phone := data.get('phone'):
-                if not phone_validation.match(request_phone):
-                    return JsonResponse({'message': f'Invalid USER, Your phone \'{request_phone}\' is worng number'}, status = 401)
-                if Accounts.objects.filter(phone = request_phone).exists():
-                    if Accounts.objects.get(phone = request_phone).password == data['password']:
-                        return JsonResponse({'message': 'Log in SUCCESS'}, status = 200)
-                    else:
-                        return JsonResponse({'message': 'Wrong PASSWORD'}, status = 200)
-                else:
-                    return JsonResponse({'message': f'Invalid USER, Your phone \'{request_phone}\' doesn\'t exist'}, status = 401)
-                
+            if bcrypt.checkpw(data['password'].encode("utf-8"), accounts.get(name = request_id).password.encode('utf-8')):
+                return JsonResponse({'message': 'Log in SUCCESS'}, status = 400)
+            
         except KeyError as error_source:
             return JsonResponse({'message': f"KEY ERROR, {error_source} is WRONG"}, status = 400)
         
         except Accounts.DoesNotExist as error_source:
-            return JsonResponse({'message': f"{data['']} is Invalid ID"}, status = 400)
-    pass
+            return JsonResponse({'message': f"{error_source} is Invalid ID"}, status = 400)
