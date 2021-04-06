bf96a0247ba3596bcf6188dd35079ba3791437fd
GyeongJae <gyeongjae.h.dev@gmail.com>
2021-04-06 09:32:42 +0900

    update: views.py if문 사이 공백으로 가독성 추가

--- students/gyeongjae-ham/westagram/users/views.py
+++ students/gyeongjae-ham/westagram/users/views.py
@@ -12,14 +12,19 @@ class SignUpView(View):
         try:
             if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'MESSAGE':'DUPLICATED_EMAIL'}, status=400)
+
             if User.objects.filter(name=data['name']).exists():
                 return JsonResponse({'MESSAGE':'DUPLICATED_NAME'}, status=400)
+
             if User.objects.filter(number=data['number']).exists():
                 return JsonResponse({'MESSAGE':'DUPLICATED_NUMBER'}, status=400)
+
             if not validator_email(data['email']):
                 return JsonResponse({'MESSAGE':'INVALIED_EMAIL'}, status=400)
+
             if not validator_password(data['password']):
                 return JsonResponse({'MESSAGE':'INVALIED_PASSWORD'}, status=400)
+
             if not validator_number(data['number']):
                 return JsonResponse({'MESSAGE':'INVALIED_NUMBER'}, status=400)
 
@@ -40,10 +45,13 @@ class LoginView(View):
         try:
             if User.objects.filter(email=data['email']).exists() == False:
                 return JsonResponse({'MESSAGE':'INVALIED_USER'}, status=401)
+
             if User.objects.filter(password=data['password']).exists() == False:
                 return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
             if User.objects.filter(email=data['email']).exists():
                 User.objects.get(email=data['email'])
+
                 if User.objects.filter(password=data['password']).exists():
                     return JsonResponse({'MESSAGE':'SUCCESS'}, status=200)
         except:
