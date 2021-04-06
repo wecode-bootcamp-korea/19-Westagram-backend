1c176b6a11465badf1acffa35ef70b4cda12d827
seungchan baek <poketsc@gmail.com>
2021-04-06 11:28:59 +0900

    identification컬럼 email,phone_number 컬럼으로 분리

--- students/seungchan-baek/user/models.py
+++ students/seungchan-baek/user/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    identification = models.CharField(max_length=50)
-    password       = models.CharField(max_length=50)
+    email          = models.CharField(max_length=50)
+    phone_number   = models.CharField(max_length=50)
+    password       = models.CharField(max_length=500)
     name           = models.CharField(max_length=50)
     nickname       = models.CharField(max_length=50)
 
