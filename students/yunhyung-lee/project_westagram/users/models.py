9843f04a8087010d1ff7a737e70095ebd5996b31
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-04-06 14:47:31 +0900

    회원가입 엔드포인트

--- students/yunhyung-lee/project_westagram/users/models.py
+++ students/yunhyung-lee/project_westagram/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class User(models.Model):
-    email       = models.EmailField(max_length=200)
-    password    = models.CharField(max_length=200)
-    phone_num   = models.CharField(max_length=11, null=True)
-    user_name   = models.CharField(max_length=20, null=True)
+    email       = models.CharField(max_length=100)
+    password    = models.CharField(max_length=100)
+    phone_num   = models.CharField(max_length=30, null=True)
+    user_name   = models.CharField(max_length=30, null=True)
 
     class Meta:
         db_table = 'users'
--- students/yunhyung-lee/project_westagram/users/views.py
+++ students/yunhyung-lee/project_westagram/users/views.py
@@ -1,41 +1,40 @@
 import json
 
-from django.db.models.fields import EmailField
 from django.http    import JsonResponse
 from django.views   import View
 
 from .models        import User
 
-MINIMUM_PASSWORD_LENGTH = 8
 
 class SignUpView(View):
     def post(self, request):
-        data        = json.loads(request.body)
-        phone_num   = str(data['phone_num'])
-        user_name   = data['user_name']
-        password    = data['password']
-        email       = data['email']
+        try:
+            data = json.loads(request.body)
+            
+            MINIMUM_PASSWORD_LENGTH = 8
 
-        if not data['email'] or not data['password']:
-            return JsonResponse({'MESSAGE' : '이메일, 패스워드가 공백입니다.'}, status=400)
+            if '@' not in data['email'] or '.' not in data['email']:
+                return JsonResponse({'MESSAGE' : 'INVALID EMAIL'}, status=400)
 
-        elif len(password) < MINIMUM_PASSWORD_LENGTH:
-            return JsonResponse({'MESSAGE' : '패스워드가 짧습니다'}, status=400)
+            if len(data['password']) < MINIMUM_PASSWORD_LENGTH:
+                return JsonResponse({'MESSAGE' : 'INVALID PASSWORD'}, status=400)
 
-        elif User.objects.filter(email=email, user_name=user_name, phone_num=phone_num):
-            return JsonResponse({'MESSAGE' : '현재 사용중입니다.'}, status=400)
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'MESSAGE' : 'DUPLICATED EMAIL'}, status=400)
         
-        elif ('@' or '.') not in data['email']:
-            return JsonResponse({'MESSAGE' : '형식에 맞지 않습니다.'}, status=400)
+            if User.objects.filter(phone_num=data['phone_num']).exists():
+                return JsonResponse({'MESSAGE' : 'DUPLICATED PHONE_NUM'}, status=400)
+            
+            if User.objects.filter(user_name=data['user_name']).exists():
+                return JsonResponse({'MESSAGE' : 'DUPLICATED USER_NAME'}, status=400)
 
-        elif not phone_num.isdigit():
-            return JsonResponse({'MESSAGE' : '숫자를 입력해주세요.'}, status=400)
-
-        User.objects.create(
-                user_name   = user_name,
-                phone_num   = phone_num,
-                password    = password,
-                email       = email
-                )
+            User.objects.create(
+                    user_name   = data['user_name'],
+                    phone_num   = data['phone_num'],
+                    password    = data['password'],
+                    email       = data['email']
+            )
         
-        return JsonResponse({'MESSAGE' : 'SUCCESS!'}, status=201)
+            return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY ERROR'}, status=400)
