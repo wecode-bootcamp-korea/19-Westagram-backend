543ce3ed01c0be027d8a6bdeebfac207b0dfc31a
seungchan baek <poketsc@gmail.com>
2021-04-06 19:23:11 +0900

    mission 3 구현 완료

--- students/seungchan-baek/user/views.py
+++ students/seungchan-baek/user/views.py
@@ -3,10 +3,10 @@ import re
 import bcrypt
 import jwt
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views       import View
+from django.http        import JsonResponse
 
-from user.models  import User
+from user.models        import User
 from westagram.settings import SECRET_KEY
 
 class SignUpView(View):
@@ -66,39 +66,40 @@ class LogInView(View):
         password = data['password']
 
         try:
+            if 'email' not in data and 'nickname' not in data and 'phone_number' not in data:
+                raise KeyError
 
             if 'email' in data:
                 email = data['email']
-                if User.objects.filter(email=email):
+                if User.objects.filter(email=email).exists():
                     user = User.objects.get(email=email)
                 else:
                     return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
             if 'nickname' in data:
                 nickname=data['nickname']
-                if User.objects.filter(nickname=nickname):
+                if User.objects.filter(nickname=nickname).exists():
                     user = User.objects.get(nickname=nickname)
                 else:
                     return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
             if 'phone_number' in data:
                 phone_number=data['phone_number']
-                if User.objects.filter(phone_number=phone_number):
+                if User.objects.filter(phone_number=phone_number).exists():
                     user = User.objects.get(phone_number=phone_number)
                 else:
                     return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
 
-            if user:
-                encode_password = user.password.encode('utf-8')
-                checked_password = bcrypt.checkpw(password.encode('utf-8'), encode_password)
-                if checked_password:
-                    access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm='HS256')
-                    return JsonResponse({'TOKEN':access_token}, status=200)
-                else:
-                    return JsonResponse({'MESSAGE':'INVALID_PASSWORD'}, status=401)
+            encode_password = user.password.encode('utf-8')
+            checked_password = bcrypt.checkpw(password.encode('utf-8'), encode_password)
+            if checked_password:
+                access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm='HS256')
+                return JsonResponse({'TOKEN':access_token}, status=200)
+            else:
+                return JsonResponse({'MESSAGE':'INVALID_PASSWORD'}, status=401)
 
         except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
 
 
 
