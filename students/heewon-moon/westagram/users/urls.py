fd2f4bd82dc1d9997735f9974bcefe74f8ead6b6
moonimooni <munhw1408@gmail.com>
2021-04-06 14:44:42 +0900

    add : 로그인 기능 구현

--- students/heewon-moon/westagram/users/urls.py
+++ students/heewon-moon/westagram/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views      import SignUpView
+from .views      import SignUpView, SignInView
 
 urlpatterns = [
     path('/sign_up', SignUpView.as_view()),
+    path('/sign_in', SignInView.as_view())
 ]
--- students/heewon-moon/westagram/users/views.py
+++ students/heewon-moon/westagram/users/views.py
@@ -1,12 +1,13 @@
-import re
 import json
+import bcrypt
+import jwt
 
 from django.views import View
 from django.http  import JsonResponse
 
 from .models      import User
 from .validators  import email_validator, password_validator, phone_validator
-
+from westagram.settings  import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
@@ -17,7 +18,6 @@ class SignUpView(View):
             password = data['password']
             nickname = data['nickname']
             phone    = data['phone']
-
 
             if User.objects.filter(email=email).exists():
                 return JsonResponse({'MESSAGE' : 'EMAIL ALREADY EXISTS'}, status=400)
@@ -31,14 +31,45 @@ class SignUpView(View):
             if not phone_validator(phone):
                 return JsonResponse({'MESSAGE' : 'INVALID PHONE NUMBER'}, status=400)
 
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
+
             User.objects.create(
                 email    = email,
-                password = password,
+                password = hashed_password,
                 nickname = nickname,
                 phone    = phone
             )
 
             return JsonResponse({'MESSAGE' : 'SUCCESS'}, status=200)
 
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY ERROR'}, status=400)
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            account  = data['account']
+            password = data['password']
+
+            if email_validator(account):
+                user = User.objects.get(email=account)
+            elif phone_validator(account):
+                user = User.objects.get(phone=account)
+            else:
+                user = User.objects.get(nickname=account)
+            
+            input_password = password.encode('utf-8')
+            db_password = user.password.encode('utf-8')
+
+            if bcrypt.checkpw(input_password, db_password):
+                token = jwt.encode({'user_id' : user.id}, SECRET_KEY, algorithm=ALGORITHM)
+                return JsonResponse({'MESSAGE' : 'SUCCESS', 'token' : token}, status=200)
+            
+            return JsonResponse({'MESSAGE' : 'WRONG PASSWORD'}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE' : 'INVALID USER'}, status=401)
         except KeyError:
             return JsonResponse({'MESSAGE' : 'KEY ERROR'}, status=400)
