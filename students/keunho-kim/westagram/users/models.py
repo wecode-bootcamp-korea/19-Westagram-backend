8a9349a240777da6bcba58af1adab8d0e2af5d82
keunho_kim <astrocat9311@gmail.com>
2021-04-06 20:53:48 +0900

    views.py editied again..

--- students/keunho-kim/westagram/users/models.py
+++ students/keunho-kim/westagram/users/models.py
@@ -1,11 +1,10 @@
-from django.db import models
+from django.db    import models
 from django.forms import PasswordInput
-
 
 class User(models.Model):
     name         = models.CharField(max_length=45)
     email        = models.CharField(max_length=100)
-    password     = models.CharField(max_length=50)
+    password     = models.TextField
     phone_number = models.CharField(max_length=20)
     nickname     = models.CharField(max_length=20)
 
--- students/keunho-kim/westagram/users/views.py
+++ students/keunho-kim/westagram/users/views.py
@@ -1,16 +1,14 @@
 import json
 import bcrypt
-from django.http import JsonResponse
+
+from django.http  import JsonResponse
 from django.views import View
 from users.models import User
 
-
-# Create your views here.
-
 class SignUpView(View):
-    def post(self, request):
+    def post(self,request):
 
-        PASSWORD_LENGTH = 8  # 편의상 상수처리.
+        PASSWORD_LENGTH = 8
 
         try:
             data = json.loads(request.body)
@@ -18,7 +16,8 @@ class SignUpView(View):
             if not "@" in data['email'] or not "." in data['email']:
                 return JsonResponse({'message': 'USE_VALID_EMAIL'}, status=400)
 
-            if User.objects.filter(email=data['email']).exists():
+            if User.objects.filter(email=data['email']).exists() or \
+                    User.objects.filter(nickname=data['nickname']).exists():
                 return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
 
             if not data['email'] or not data['password'] or not data['nickname']:
@@ -27,33 +26,40 @@ class SignUpView(View):
             if len(data['password']) < PASSWORD_LENGTH:
                 return JsonResponse({'message': 'SHORT_PASSWORD'}, status=400)
 
+            hashed_password = bcrypt.hashpw(data['password'].encode('UTF-8'), bcrypt.gensalt()).decode()
+
             User.objects.create(
-                email=data['email'],
-                password=data['password'],
-                phone_number=data['phone_number'],
-                name=data['name'],
-                nickname=data['nickname'],
+                email        = data['email'],
+                password     = hashed_password,
+                phone_number = data['phone_number'],
+                name         = data['name'],
+                nickname     = data['nickname'],
             )
             return JsonResponse({'message': 'SUCCESS'}, status=201)
-
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-
 class LogInView(View):
-    def post(self, request):
+    def post(self,request):
 
         try:
             data = json.loads(request.body)
 
             if User.objects.filter(email=data['email']).exists() and \
                     User.objects.filter(password=data['password']).exists():
-                return JsonResponse({'message': 'LOG_IN_SUCCESS'}, status=200)
+                return JsonResponse({'message': 'LOG_IN_SUCCESS'}, status=201)
+
+            if User.objects.filter(nickname=data['nickname']).exists() and \
+                    User.objects.filter(password=data['password']).exists():
+                return JsonResponse({'message': 'LOG_IN_SUCCESS'}, status=201)
+
+            if User.objects.filter(phone_number=data['phone_number']).exists() and \
+                    User.objects.filter(password=data['password']).exists():
+                return JsonResponse({'message': 'LOG_IN_SUCCESS'}, status=201)
+
             else:
-                return JsonResponse({'message': 'ACCESS_DENIED'})
-
-
+                return JsonResponse({'message': 'ACCESS_DENIED'}, status=400)
 
         except KeyError:
             return JsonResponse({'message': 'Key_Error!'}, status=400)
