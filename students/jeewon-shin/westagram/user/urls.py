60bff99e1f2697ff98d2bc08b7ed48c53e70f246
jeewonn <jeewonn0504@gmail.com>
2021-04-06 19:00:34 +0900

    체크포인트3 ver1

--- students/jeewon-shin/westagram/user/urls.py
+++ students/jeewon-shin/westagram/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from user.views import SignupView
+from user.views import SignupView,LoginView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
+    path('/login', LoginView.as_view()),
 ]
--- students/jeewon-shin/westagram/user/views.py
+++ students/jeewon-shin/westagram/user/views.py
@@ -15,10 +15,10 @@ class SignupView(View):
             if '@' not in data['email'] or '.' not in data['email']:
                 return JsonResponse({'MESSGAGE': 'INVALID EMAIL'}, status=400)
 
-            if User.objects.filter(email= data['email']).exists():
+            if User.objects.filter(email=data['email']).exists():
                 return JsonResponse({'MESSGAGE': 'ALREADY USED'}, status=400)
 
-            if User.objects.filter(user_name=data['user_name']).exists():
+            if User.objects.filter(nickname=data['nickname']).exists():
                 return JsonResponse({'MESSGAGE': 'ALREADY USED'}, status=400)
 
             if len(data['password']) <= PASSWORD_LENGTH:
@@ -34,7 +34,42 @@ class SignupView(View):
                 phone_number = data['phone_number'],
                 password     = decode_hash_pw
             )
+
             return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
 
         except KeyError:
             return JsonResponse({'MESSAGE': 'KEYERROR'}, status=400)
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email = data.get('email')
+            nickname = data.get('nickname')
+            phone_number = data.get('phone_number')
+
+            if email:
+                if User.objects.filter(email=data['email']).exists():
+                    passwords = User.objects.get(email=data['email']).password
+                else:
+                    return JsonResponse({'MESSAGE': 'NO VALIED'}, status=401)
+            if nickname:
+                if User.objects.filter(nickname=data['nickname']).exists():
+                    passwords = User.objects.get(nickname=data['nickname']).password
+                else:
+                    return JsonResponse({'MESSAGE': 'NO VALIED'}, status=401)
+            if phone_number:
+                if User.objects.filter(phone_number=data['phone_number']).exists():
+                    passwords = User.objects.get(phone_number=data['phone_number']).password
+                else:
+                    return JsonResponse({'MESSAGE': 'NO VALIED'}, status=401)
+
+            bcrypt.checkpw(data['password'].encode('utf-8'),passwords.encode('utf-8'))
+            return JsonResponse({'MESSGAGE': 'SUCCESS'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE': 'KEY_ERROR'}, status=400)
+
+
+
