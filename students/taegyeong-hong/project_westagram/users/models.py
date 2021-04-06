2e81c9b9922a776a841917498f5528b0183fd0ee
ubuntu <a01026676497@gmail.com>
2021-04-06 20:18:10 +0900

    seccess mission2

--- students/taegyeong-hong/project_westagram/users/models.py
+++ students/taegyeong-hong/project_westagram/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    phone_num = models.CharField(max_length=50)
+    class meta:
+        db_table ='users'
--- students/taegyeong-hong/project_westagram/users/views.py
+++ students/taegyeong-hong/project_westagram/users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views import View
+from .models import User
+
+class SignupView(View):
+    def post(self, request):
+        requestpassword = 8
+        
+        try:
+
+            data = json.loads(request.body)
+
+            if '@' not in data['email'] or '.' not in data['email']:
+                return JsonResponse({'message':'email validation'},status=400)
+
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message':'DUPLICATED EMAIL'},status=400)
+            if len(data['password']) <= requestpassword :
+                return JsonResponse({'message':'INVALID PASSWORD'},status=400)
+
+            User.objects.create(
+                email =data['email'],
+                password = data['password'],
+                name= data['name'],
+                phone_num =data['phone_num']
+            )
+            return JsonResponse({'message':'SUCCESS'},stats=201)
+
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'},status=400)
 
 
